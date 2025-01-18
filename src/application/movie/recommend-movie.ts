0b6903e7c79baef6143bffef19e1f806eced7f78
imdhemy <imdhemy@gmail.com>
2025-01-18 19:40:05 +0100

    chore: code refactoring

--- src/application/movie/recommend-movie.ts
+++ src/application/movie/recommend-movie.ts
@@ -2,30 +2,16 @@ import { IMovie, IMovieProps } from './types';
 import { db } from '../../infrastructure/database';
 
 export async function recommendMovie(props: Partial<IMovieProps>): Promise<IMovie[]> {
-    const aggCursor = db.collection('movies').aggregate([
-        {
-            $search: {
-                moreLikeThis: {
-                    like: props,
-                }
-            }
-        },
-        {
-            $limit: 5
-        },
-        {
-            $addFields: {
-                score: { $meta: 'searchScore' }
-            }
-        }
+    const cursor = db.collection('movies').aggregate([
+        { $search: { moreLikeThis: { like: props } } },
+        { $limit: 5 },
+        { $addFields: { score: { $meta: 'searchScore' } } }
     ]);
-    const result = new Set<IMovie>();
 
-    for await (const movie of aggCursor) {
-        movie.id = movie._id.toString();
-        delete movie._id;
-        result.add(movie as IMovie);
+    const result: IMovie[] = [];
+    for await (const movie of cursor) {
+        result.push({ ...movie, id: movie._id.toString() } as IMovie);
     }
 
-    return Array.from(result);
+    return result;
 }
