9f57f0354fcfd4b9385fe3792d10861bfc2bf3c7
imdhemy <imdhemy@gmail.com>
2025-01-18 19:36:39 +0100

    chore: code refactoring

--- src/application/movie/create-movie.ts
+++ src/application/movie/create-movie.ts
@@ -2,10 +2,7 @@ import { IMovie, IMovieProps } from './types';
 import { db } from '../../infrastructure/database';
 
 export async function createMovie(props: IMovieProps): Promise<IMovie> {
-    const doc: Partial<IMovie> = {
-        ...props,
-        releaseDate: new Date(props.releaseDate),
-    };
+    const doc: Partial<IMovie> = { ...props, releaseDate: new Date(props.releaseDate) };
 
     const result = await db.collection('movies').insertOne({ ...doc });
     doc.id = result.insertedId.toString();
